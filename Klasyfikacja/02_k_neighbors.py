e5ac83e1087face34625e00e0b16c513d7569809
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-03-05 22:07:39 +0100

    Porównanie wykresów

--- Klasyfikacja/02_k_neighbors.py
+++ Klasyfikacja/02_k_neighbors.py
@@ -50,4 +50,91 @@ plt.ylabel('cecha_2: sepal_width')
 plt.show()
 
 df = pd.DataFrame(data=np.c_[data, target], columns=['sepal_length', 'sepal_width', 'class'])
-px.scatter(df, x='sepal_length', y='sepal_width', color='class', width=800)
+fig = px.scatter(df, x='sepal_length', y='sepal_width', color='class', width=800)
+fig.show()
+
+'''
+K-nearest Neighbour Algorithm - Algorytm K-najbliższych sąsiadów
+
+Podstawą działania algorytmu jest znalezienie z góry określonej liczby próbek treningowych 
+znajdujących się najbliżej naszej obserwacji i przewidzenie na ich podstawie etykiety.
+
+Liczba sąsiadów jest określana przez użytkownika. Odległości zwykle kalkuluje sie przy pomocy 
+metryki euklidesowej.
+'''
+
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(data, target)
+
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+                     metric_params=None, n_jobs=None, n_neighbors=5, p=2,
+                     weights='uniform')
+
+# Wykres granic decyzyjnych
+
+x_min, x_max = data[:, 0].min() - 0.5, data[:, 0].max() + 0.5
+y_min, y_max = data[:, 1].min() - 0.5, data[:, 1].max() + 0.5
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+mesh = np.c_[xx.ravel(), yy.ravel()]
+Z = classifier.predict(mesh)
+Z = Z.reshape(xx.shape)
+
+plt.figure(figsize=(10, 8))
+plt.pcolormesh(xx, yy, Z, cmap='gnuplot', alpha=0.1)
+plt.scatter(data[:, 0], data[:, 1], c=target, cmap='gnuplot', edgecolors='r')
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+plt.title('3-class classification k=5')
+plt.show()
+
+
+def plot_decision_boundries(n_neighbors=1):
+    classifier = KNeighborsClassifier(n_neighbors=n_neighbors)
+    classifier.fit(data, target)
+
+    x_min, x_max = data[:, 0].min() - 0.5, data[:, 0].max() + 0.5
+    y_min, y_max = data[:, 1].min() - 0.5, data[:, 1].max() + 0.5
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+    mesh = np.c_[xx.ravel(), yy.ravel()]
+    Z = classifier.predict(mesh)
+    Z = Z.reshape(xx.shape)
+
+    plt.figure(figsize=(10, 8))
+    plt.pcolormesh(xx, yy, Z, cmap='gnuplot', alpha=0.1)
+    plt.scatter(data[:, 0], data[:, 1], c=target, cmap='gnuplot', edgecolors='r')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title(f'3-class classification k={n_neighbors}')
+    plt.show()
+
+
+plot_decision_boundries()
+
+
+plot_decision_boundries(2)
+plot_decision_boundries(50)
+
+plt.figure(figsize=(12, 12))
+
+for i in range(1, 7):
+    plt.subplot(3, 2, i)
+
+    clf = KNeighborsClassifier(n_neighbors=i)
+    clf.fit(data, target)
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+    mesh = np.c_[xx.ravel(), yy.ravel()]
+    Z = clf.predict(mesh)
+    Z = Z.reshape(xx.shape)
+
+    plt.pcolormesh(xx, yy, Z, cmap='gnuplot', alpha=0.1)
+    plt.scatter(data[:, 0], data[:, 1], c=target, cmap='gnuplot', edgecolors='r')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+    plt.title(f'3-class classification k={i}')
+
+plt.show()
