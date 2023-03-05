ce7c440e789cf25e003459e6e42421d572fa1a49
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-03-05 21:53:45 +0100

    Wykresy porównania

--- Klasyfikacja/02_k_neighbors.py
+++ Klasyfikacja/02_k_neighbors.py
@@ -28,3 +28,26 @@ print(df.head())
 print(df.info())
 print(df.describe().T)
 print(df['class'].value_counts())
+
+# Wykres Rozproszenia
+
+print(df.columns)
+
+_ = sns.pairplot(df, vars=all_data['feature_names'], hue='class')
+plt.show()
+print(df.corr())
+
+data = data[:, :2]
+
+print('data shape:', data.shape)
+print('target shape:', target.shape)
+
+plt.figure(figsize=(8, 6))
+plt.scatter(data[:, 0], data[:, 1], c=target, cmap='viridis')
+plt.title('Wykres punktowy')
+plt.xlabel('cecha_1: sepal_length')
+plt.ylabel('cecha_2: sepal_width')
+plt.show()
+
+df = pd.DataFrame(data=np.c_[data, target], columns=['sepal_length', 'sepal_width', 'class'])
+px.scatter(df, x='sepal_length', y='sepal_width', color='class', width=800)
