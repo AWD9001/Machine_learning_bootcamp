8e0f7acaa078011ed2d695c37b5358ca12551e6a
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-03-21 22:32:13 +0100

    Wskaźnik Giniego

--- Klasyfikacja/03_gini_entropy.py
+++ Klasyfikacja/03_gini_entropy.py
@@ -7,3 +7,12 @@ import seaborn as sns
 from IPython.display import Image
 
 sns.set(font_scale=1.3)
+
+# wskaźnik Giniego
+
+
+1 - (50 / 150)**2 - (50 / 150)**2 - (50 / 150)**2  # dla korzenia - poziom 0
+
+1 - (45 / 52)**2 - (6 / 52)**2 - (1 / 52)**2  # poziom 1 - węzeł lewy
+
+1 - (5 / 98)**2 - (44 / 98)**2 - (49 / 98)**2  # poziom 1 - węzeł prawy
