62b38e8db70f9fc2d1e985544e0c1b8c9aa9c1c5
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-02 23:32:41 +0200

    Usunięto niepotrzebne biblioteki

--- Klasyfikacja/03_gini_entropy.py
+++ Klasyfikacja/03_gini_entropy.py
@@ -1,10 +1,8 @@
 # drzewa decyzyjne
 
 import numpy as np
-# import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# from IPython.display import Image
 
 sns.set(font_scale=1.3)
 
