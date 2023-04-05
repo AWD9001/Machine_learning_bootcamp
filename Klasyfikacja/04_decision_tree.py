53fc9264eeaf4685339b4cce3cd8762d5486c38d
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-05 22:27:13 +0200

    Załadowanie danych

--- Klasyfikacja/04_decision_tree.py
+++ Klasyfikacja/04_decision_tree.py
@@ -8,3 +8,17 @@ from IPython.display import Image
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
+
+# Załadowanie danych
+raw_data = load_iris()
+all_data = raw_data.copy()
+
+data = all_data['data']
+target = all_data['target']
+feature_names = [name.replace(' ', '_')[:-5] for name in all_data['feature_names']]
+target_names = all_data['target_names']
+
+print(f'Liczba próbek: {len(data)}')
+print(f'Kształt danych: {data.shape}')
+print(f'Nazwy zmiennych objaśniających: {feature_names}')
+print(f'Nazwy kategorii kosaćca: {target_names}')
