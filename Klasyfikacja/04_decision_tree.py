1ffc82d61b3f6471c71f6ac9602ba6ea11b1ed2d
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-15 22:43:44 +0200

    Zmiana kolejności bibliotek na alfabetyczną

--- Klasyfikacja/04_decision_tree.py
+++ Klasyfikacja/04_decision_tree.py
@@ -1,10 +1,10 @@
 # import bibliotek
+from IPython.display import Image
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.datasets import load_iris
-from IPython.display import Image
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
