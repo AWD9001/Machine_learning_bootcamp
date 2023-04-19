d005692e1bf14bfccbc147b08e6d110b85f80dc8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-19 21:43:09 +0200

    Import bibliotek

---
+++ Klasyfikacja/05_random_forest.py
@@ -0,0 +1,9 @@
+# Import bibliotek
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+sns.set(font_scale=1.3)
+np.random.seed(42)
