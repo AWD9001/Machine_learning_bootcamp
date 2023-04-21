3d477960d6ccad0ae22e14ae3cd005b58ad709a3
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-21 22:25:55 +0200

    Załadowanie danych

--- Klasyfikacja/05_random_forest.py
+++ Klasyfikacja/05_random_forest.py
@@ -7,3 +7,15 @@ import plotly.express as px
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
+
+# Załadowanie danych
+from sklearn.datasets import load_iris
+
+raw_data = load_iris()
+all_data = raw_data.copy()
+
+data = all_data['data']
+target = all_data['target']
+feature_names = all_data['feature_names']
+target_names = all_data['target_names']
+
