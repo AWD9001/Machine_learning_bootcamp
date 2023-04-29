c7e41a0e1c6364965a760127feeb6324d5174a14
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-04-29 23:38:58 +0200

    Wygenerowanie danych

--- Klasyfikacja/06_svc.py
+++ Klasyfikacja/06_svc.py
@@ -5,3 +5,18 @@ import seaborn as sns
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
+
+# Wygenerowanie danych
+from sklearn.datasets import load_iris
+
+raw_data = load_iris()
+all_data = raw_data.copy()
+
+data = all_data['data']
+target = all_data['target']
+feature_names = all_data['feature_names']
+target_names = all_data['target_names']
+
+df = pd.DataFrame(np.c_[data, target], columns=feature_names + ['target'])
+# df = df[(df['target'] == 0.0) | (df['target'] == 1.0)]
+df.head()
