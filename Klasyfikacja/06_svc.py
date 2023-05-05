ac6cba0d6ba15df5de2812177a9faefe9f6b30c8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-05-05 22:21:57 +0200

    Utworzenie wykresów do SVM - linear

--- Klasyfikacja/06_svc.py
+++ Klasyfikacja/06_svc.py
@@ -49,3 +49,19 @@ classifier.fit(X_train, y_train)
 
 classifier.score(X_test, y_test)
 
+from mlxtend.plotting import plot_decision_regions
+
+plt.figure(figsize=(8, 6))
+plot_decision_regions(X_train, y_train, classifier)
+plt.xlabel(feature_names[2])
+plt.ylabel(feature_names[1])
+plt.title(f'SVC: train accuracy: {classifier.score(X_train, y_train):.4f}')
+plt.show()
+
+plt.figure(figsize=(8, 6))
+plot_decision_regions(X_test, y_test, classifier)
+plt.xlabel(feature_names[2])
+plt.ylabel(feature_names[1])
+plt.title(f'SVC: test accuracy: {classifier.score(X_test, y_test):.4f}')
+plt.show()
+
