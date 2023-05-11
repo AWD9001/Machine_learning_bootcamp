ec109479336cf761f36ddca372607e2040defd5a
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-05-11 21:55:25 +0200

    Import bibliotek

---
+++ Klasyfikacja/07_naive_bayes.py
@@ -0,0 +1,8 @@
+# Import bibliotek
+import numpy as np
+import pandas as pd
+import sklearn
+
+np.random.seed(42)
+np.set_printoptions(precision=6, suppress=True)
+print(f'sklearn version is: {sklearn.__version__}', '\n')
