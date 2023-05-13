99833dcb413ff5d17c00c2ea0fe829c94849a5d4
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-05-13 22:30:15 +0200

    Wygenerowanie danych

--- Klasyfikacja/07_naive_bayes.py
+++ Klasyfikacja/07_naive_bayes.py
@@ -6,3 +6,15 @@ import sklearn
 np.random.seed(42)
 np.set_printoptions(precision=6, suppress=True)
 print(f'sklearn version is: {sklearn.__version__}', '\n')
+
+# Wygenerowanie danych
+pogoda = ['słonecznie', 'deszczowo', 'pochmurno', 'deszczowo', 'słonecznie', 'słonecznie',
+          'pochmurno', 'pochmurno', 'słonecznie']
+temperatura = ['ciepło', 'zimno', 'ciepło', 'ciepło', 'ciepło', 'umiarkowanie',
+               'umiarkowanie', 'ciepło', 'zimno']
+
+spacer = ['tak', 'nie', 'tak', 'nie', 'tak', 'tak', 'nie', 'tak', 'nie']
+
+raw_df = pd.DataFrame(data={'pogoda': pogoda, 'temperatura': temperatura, 'spacer': spacer})
+df = raw_df.copy()
+print(df)
