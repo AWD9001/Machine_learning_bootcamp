158d769f1d73e2c80ec7f873dab4f352bd75411e
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-05-22 21:53:47 +0200

    Metryki - Klasyfikacja binarna

--- Klasyfikacja/08_classification_metrics.py
+++ Klasyfikacja/08_classification_metrics.py
@@ -2,5 +2,30 @@
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.express as px
+# import plotly.express as px
 from plotly.subplots import make_subplots
+
+# Metryki - Klasyfikacja binarna
+y_true = np.array([1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+                   1, 0, 1])
+y_pred = np.array([0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
+                   1, 0, 1])
+
+from sklearn.metrics import accuracy_score
+accuracy_score(y_true, y_pred)
+
+results = pd.DataFrame({'y_true': y_true, 'y_pred': y_pred})
+print(results, '\n')
+
+results = results.sort_values(by='y_true')
+results = results.reset_index(drop=True)
+results['sample'] = results.index + 1
+print(results)
+
+fig = make_subplots(rows=2, cols=1)
+fig.add_trace(go.Scatter(x=results['sample'], y=results['y_true'], mode='markers', name='y_true'),
+              row=1, col=1)
+fig.add_trace(go.Scatter(x=results['sample'], y=results['y_pred'], mode='markers', name='y_pred'),
+              row=2, col=1)
+fig.update_layout(width=800, height=600, title='Klasyfikator binarny')
+fig.show()
