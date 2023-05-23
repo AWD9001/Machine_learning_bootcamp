5b5f47efdf4e5908aa0deceb8f0f39c0225c801b
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-05-23 22:22:22 +0200

    Macierz konfuzji/pomyłek cz.1

--- Klasyfikacja/08_classification_metrics.py
+++ Klasyfikacja/08_classification_metrics.py
@@ -29,3 +29,24 @@ fig.add_trace(go.Scatter(x=results['sample'], y=results['y_pred'], mode='markers
               row=2, col=1)
 fig.update_layout(width=800, height=600, title='Klasyfikator binarny')
 fig.show()
+
+# Macierz konfuzji/pomyłek
+from sklearn.metrics import confusion_matrix
+
+cm = confusion_matrix(y_true, y_pred)
+print(cm)
+
+import plotly.figure_factory as ff
+
+
+def plot_confusion_matrix(cmm):
+    cmm = cmm[::-1]
+    cmm = pd.DataFrame(cmm, columns=['pred_0', 'pred_1'], index=['true_1', 'true_0'])
+
+    figu = ff.create_annotated_heatmap(z=cmm.values, x=list(cmm.columns), y=list(cmm.index),
+                                       colorscale='ice', showscale=True, reversescale=True)
+    figu.update_layout(width=400, height=400, title='Confusion Matrix', font_size=16)
+    figu.show()
+
+
+plot_confusion_matrix(cm)
