14e54bdd2345da6298024162de7993eb4e187e82
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-06-11 22:34:01 +0200

    Wygenerowanie danych

--- Nadzorowanie/01 Podstawy/01_preprocessing.py
+++ Nadzorowanie/01 Podstawy/01_preprocessing.py
@@ -1,7 +1,20 @@
 # Import bibliotek
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 import sklearn
 
 print(sklearn.__version__)
+
+# Wygenerowanie danych
+data = {
+    'size': ['XL', 'L', 'M', 'L', 'M'],
+    'color': ['red', 'green', 'blue', 'green', 'red'],
+    'gender': ['female', 'male', 'male', 'female', 'female'],
+    'price': [199.0, 89.0, 99.0, 129.0, 79.0],
+    'weight': [500, 450, 300, 380, 410],
+    'bought': ['yes', 'no', 'yes', 'no', 'yes']
+}
+
+df_raw = pd.DataFrame(data=data)
+print(df_raw)
