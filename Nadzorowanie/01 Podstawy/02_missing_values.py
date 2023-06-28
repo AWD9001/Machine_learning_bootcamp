1181e297601aea2dd3416cf939cad61e9ad2f534
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-06-28 22:13:48 +0200

    Wygenerowanie danych

--- Nadzorowanie/01 Podstawy/02_missing_values.py
+++ Nadzorowanie/01 Podstawy/02_missing_values.py
@@ -4,3 +4,16 @@ import pandas as pd
 import sklearn
 
 print(sklearn.__version__)
+
+# Wygenerowanie danych
+data = {
+    'size': ['XL', 'L', 'M', np.nan, 'M', 'M'],
+    'color': ['red', 'green', 'blue', 'green', 'red', 'green'],
+    'gender': ['female', 'male', np.nan, 'female', 'female', 'male'],
+    'price': [199.0, 89.0, np.nan, 129.0, 79.0, 89.0],
+    'weight': [500, 450, 300, np.nan, 410, np.nan],
+    'bought': ['yes', 'no', 'yes', 'no', 'yes', 'no']
+}
+
+df_raw = pd.DataFrame(data=data)
+print(df_raw)
