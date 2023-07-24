dd54d4df85d4cb9ccccd16d867c5538bc5b07a2b
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-07-24 22:43:19 +0200

    Braki danych w szeregach czasowych 10

--- Nadzorowanie/01 Podstawy/02_missing_values.py
+++ Nadzorowanie/01 Podstawy/02_missing_values.py
@@ -1,6 +1,7 @@
 # Import bibliotek
 import numpy as np
 import pandas as pd
+from pandas.plotting import register_matplotlib_converters
 import plotly.express as px
 import sklearn
 from sklearn.impute import SimpleImputer
@@ -89,7 +90,6 @@ date_range = pd.date_range(start='01-01-2020 09:00', end='01-01-2020 18:00', per
 df = pd.DataFrame(data=data, index=date_range)
 print(df)
 
-from pandas.plotting import register_matplotlib_converters
 import matplotlib.pyplot as plt
 import seaborn as sns
 register_matplotlib_converters()
