4c327e499b06cb4a05793292b2301aab791f8530
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-07-30 23:05:36 +0200

    Załadowanie danych

--- Nadzorowanie/01 Podstawy/03_feature_extraction.py
+++ Nadzorowanie/01 Podstawy/03_feature_extraction.py
@@ -2,3 +2,17 @@
 import numpy as np
 import pandas as pd
 import sklearn
+
+# Załadowanie danych
+
+
+def fetch_financial_data(company='AMZN'):
+    """
+    This function fetches stock market quotations.
+    """
+    import pandas_datareader.data as web
+    return web.DataReader(name=company, data_source='stooq')
+
+
+df_raw = fetch_financial_data()
+df_raw.head()
