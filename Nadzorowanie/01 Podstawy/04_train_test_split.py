570ac5bdbb6af2cc780c9494dbb93192d0ffa7b8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-08-08 21:09:37 +0200

    Załadowanie danych

--- Nadzorowanie/01 Podstawy/04_train_test_split.py
+++ Nadzorowanie/01 Podstawy/04_train_test_split.py
@@ -9,3 +9,12 @@ np.set_printoptions(edgeitems=30, linewidth=100000,
                     formatter=dict(float=lambda x: f'{x:.2f}'))
 sns.set()
 print(sklearn.__version__)
+
+# Załadowanie danych
+from sklearn.datasets import load_iris
+
+raw_data = load_iris()
+raw_data_copy = raw_data.copy()
+raw_data_copy.keys()
+
+print(raw_data_copy['DESCR'])
