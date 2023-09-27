c3a94d20cd08f52d622a02cb590c7a3b220465f2
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-09-27 22:26:56 +0200

    Wygenerowanie danych

--- Nadzorowanie/02 Regresja/02_gradient_opadajacy.py
+++ Nadzorowanie/02 Regresja/02_gradient_opadajacy.py
@@ -4,3 +4,12 @@ import numpy as np
 # import plotly.express as px
 
 np.random.seed(42)
+
+# Wygenerowanie danych
+X1 = np.array([1, 2, 3, 4, 5, 6])
+Y = np.array([3000, 3250, 3500, 3750, 4000, 4250])
+m = len(X1)
+
+print(f'Lata pracy: {X1}')
+print(f'Wynagrodzenie: {Y}')
+print(f'Liczba próbek: {m}\n')
