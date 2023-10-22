cf86c825e226fd4219898e8818688f5c1123c7a2
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-10-22 22:09:32 +0200

    Regresja liniowa - zbiór testowy - wizualizacja

--- Nadzorowanie/02 Regresja/03_regresja_liniowa_R2.py
+++ Nadzorowanie/02 Regresja/03_regresja_liniowa_R2.py
@@ -1,9 +1,9 @@
 # Import bibliotek
 import matplotlib.pyplot as plt
 import numpy as np
-# import pandas as pd
 import seaborn as sns
 import sklearn
+from sklearn.datasets import make_regression
 
 np.random.seed(42)
 np.set_printoptions(precision=6, suppress=True, edgeitems=30, linewidth=120,
@@ -12,8 +12,6 @@ sns.set(font_scale=1.3)
 print(f'sklearn.__version__: {sklearn.__version__}\n')
 
 # Wygenerowanie danych
-from sklearn.datasets import make_regression
-
 data, target = make_regression(n_samples=100, n_features=1, n_targets=1, noise=30.0,
                                random_state=42)
 
