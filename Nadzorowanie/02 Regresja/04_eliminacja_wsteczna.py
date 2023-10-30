a9034c16290692647e05b900f0cd6393051692f5
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-10-30 21:58:39 +0100

    Import bibliotek

---
+++ Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
@@ -0,0 +1,11 @@
+# Import bibliotek
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+
+sns.set()
+np.random.seed(42)
+np.set_printoptions(precision=4, suppress=True)
+print(f'sklearn version: {sklearn.__version__}')
