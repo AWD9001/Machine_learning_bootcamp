7864687b37162b03bb6267dda4fdb8dddacebfc0
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-10-31 22:39:20 +0100

    Wczytanie danych

--- Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
+++ Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
@@ -9,3 +9,8 @@ sns.set()
 np.random.seed(42)
 np.set_printoptions(precision=4, suppress=True)
 print(f'sklearn version: {sklearn.__version__}')
+
+# Wczytanie danych
+df_raw = pd.read_csv('https://storage.googleapis.com/esmartdata-courses-files/'
+                     'ml-course/insurance.csv')
+print(df_raw.head())
