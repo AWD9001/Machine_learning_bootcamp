29425816af3366a0d16c0dfeb6f1be8dda1d8dea
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-11-27 23:14:38 +0100

    Dobór cech modelu - eliminacja wsteczna 4

--- Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
+++ Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
@@ -131,3 +131,9 @@ predictors.remove('sex_male')
 
 ols = sm.OLS(endog=y_train, exog=X_selected).fit()
 print(ols.summary(xname=predictors))
+
+X_selected = X_train_ols[:, [0, 1, 2, 3, 5, 7, 8]]
+predictors.remove('region_northwest')
+
+ols = sm.OLS(endog=y_train, exog=X_selected).fit()
+print(ols.summary(xname=predictors))
