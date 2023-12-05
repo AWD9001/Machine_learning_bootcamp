f351588668b8be7e0f9cc46dfbfffb787927da18
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-12-05 23:51:02 +0100

    Zapisanie końcowego modelu

--- Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
+++ Nadzorowanie/02 Regresja/04_eliminacja_wsteczna.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import seaborn as sns
 import sklearn
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 sns.set()
@@ -100,8 +101,6 @@ predictions['error'] = predictions['y_true'] - predictions['y_pred']
 print(predictions.head())
 
 predictions.error.plot(kind='hist', bins=30)
-
-from sklearn.metrics import mean_absolute_error
 
 mae = mean_absolute_error(y_true, y_pred)
 print(f'MAE wynosi: {mae:.2f}')
