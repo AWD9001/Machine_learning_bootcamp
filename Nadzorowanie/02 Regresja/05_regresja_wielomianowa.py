e205f21d8c8904255c7b65b59999818fd90065f9
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-12-08 23:19:12 +0100

    Import bibliotek

---
+++ Nadzorowanie/02 Regresja/05_regresja_wielomianowa.py
@@ -0,0 +1,12 @@
+# Import bibliotek
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+# import plotly.express as px
+import sklearn
+
+np.random.seed(42)
+np.set_printoptions(precision=6, suppress=True)
+sns.set(font_scale=1.3)
+print(sklearn.__version__)
