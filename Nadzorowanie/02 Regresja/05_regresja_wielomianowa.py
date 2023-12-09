eec17ce34c19eb957fee1a96f8d22de1a0cc9974
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-12-09 23:37:47 +0100

    Wygenerowanie danych

--- Nadzorowanie/02 Regresja/05_regresja_wielomianowa.py
+++ Nadzorowanie/02 Regresja/05_regresja_wielomianowa.py
@@ -1,7 +1,7 @@
 # Import bibliotek
 import numpy as np
 # import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 # import plotly.express as px
 import sklearn
@@ -10,3 +10,32 @@ np.random.seed(42)
 np.set_printoptions(precision=6, suppress=True)
 sns.set(font_scale=1.3)
 print(sklearn.__version__)
+
+"""
+Polynomial Regression - Regresja Wielomianowa
+Powszechnym wzorcem stosowanym w uczeniu maszynowym jest użycie modeli liniowych wytrenowanych na
+nieliniowych funkcjach danych. Podejscie to utrzymuje szybkie działanie metod liniowych i zarazem
+umożliwia stosowanie metod liniowych dla znacznie szerszego zakresu danych.
+
+Przykładowo, prosta regresja liniowa może zostać zastosowana przez skonstruowanie cech
+wielomianowych do modelów nieliniowych.
+
+To implikuje fakt, iż nasz model regresji wielomianowej znajduje się w klasie modeli liniowych i
+może być rozwiązany za pomocą tych samych technik.
+
+Rozważmy model regresji liniowej: Y = w0 + w1X1 
+"""
+
+# Wygenerowanie danych
+X = np.arange(-10, 10, 0.5)
+noise = 80 * np.random.randn(40)
+y = -X**3 + 10*X**2 - 2*X + 3 + noise
+X = X.reshape(40, 1)
+
+plt.figure(figsize=(8, 6))
+plt.title('Regresja wielomianowa')
+plt.xlabel('cecha x')
+plt.ylabel('zmienna docelowa')
+plt.scatter(X, y, label='cecha x')
+plt.legend()
+plt.show()
