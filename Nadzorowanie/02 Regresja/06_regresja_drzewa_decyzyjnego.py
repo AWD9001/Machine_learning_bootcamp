843e390b7b7bec7c8823187ef5395ef496d5b433
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2023-12-29 23:23:30 +0100

    Regresja drzew decyzyjnych

--- Nadzorowanie/02 Regresja/06_regresja_drzewa_decyzyjnego.py
+++ Nadzorowanie/02 Regresja/06_regresja_drzewa_decyzyjnego.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import sklearn
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
@@ -36,6 +37,19 @@ plot_data = np.arange(-3, 3, 0.01).reshape(-1, 1)
 plt.figure(figsize=(8, 6))
 plt.title('Regresja liniowa')
 plt.plot(plot_data, regressor.predict(plot_data), c='red', label='regresja liniowa')
+plt.scatter(data, target, label='dane')
+plt.legend()
+plt.xlabel('cecha x')
+plt.ylabel('target')
+plt.show()
+
+# Regresja drzew decyzyjnych
+regressor = DecisionTreeRegressor()
+regressor.fit(data, target)
+
+plt.figure(figsize=(8, 6))
+plt.title('Regresja drzew decyzyjnych')
+plt.plot(plot_data, regressor.predict(plot_data), c='green', label=f'regresja drzew decyzyjnych')
 plt.scatter(data, target, label='dane')
 plt.legend()
 plt.xlabel('cecha x')
