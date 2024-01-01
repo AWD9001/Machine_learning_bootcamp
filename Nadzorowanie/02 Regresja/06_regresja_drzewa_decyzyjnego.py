a6f9f73bd2de603d6622a1687cd21536fe52683a
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-01-01 23:00:25 +0100

    Graf drzewa decyzyjnego

--- Nadzorowanie/02 Regresja/06_regresja_drzewa_decyzyjnego.py
+++ Nadzorowanie/02 Regresja/06_regresja_drzewa_decyzyjnego.py
@@ -1,16 +1,21 @@
 # Import bibliotek
+from IPython.display import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import pydotplus
 import seaborn as sns
 import sklearn
 from sklearn.datasets import make_regression
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import export_graphviz
 from sklearn.tree import DecisionTreeRegressor
+from six import StringIO
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
 np.set_printoptions(precision=6, suppress=True)
 print(sklearn.__version__)
+
 
 # Wygenerowanie danych
 data, target = make_regression(n_samples=200, n_features=1, noise=20)
@@ -18,6 +23,7 @@ target = target ** 2
 
 print(f'{data[:5]}\n')
 print(target[:5])
+
 
 # Regresja liniowa
 plt.figure(figsize=(8, 6))
@@ -83,3 +89,15 @@ plt.legend()
 plt.xlabel('cecha x')
 plt.ylabel('target')
 plt.show()
+
+
+# Graf drzewa decyzyjnego
+dot_data = StringIO()
+export_graphviz(regressor, out_file=dot_data,
+                filled=True, rounded=True,
+                special_characters=True,
+                feature_names=['cecha x'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('graph.png')
+
+Image(graph.create_png(), width=600)
