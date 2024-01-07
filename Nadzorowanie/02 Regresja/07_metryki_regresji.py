2baf8e17d20419f81ef299f842603c92fb71418c
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-01-07 21:53:09 +0100

    Import bibliotek

--- Nadzorowanie/02 Regresja/07_metryki_regresji.py
+++ Nadzorowanie/02 Regresja/07_metryki_regresji.py
@@ -1,7 +1,10 @@
 # Import bibliotek
 import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+# import pandas as pd
+# import plotly.express as px
+# import plotly.graph_objects as go
 
 np.random.seed(42)
+
+y_true = 100 + 20 * np.random.randn(50)
+print(y_true)
