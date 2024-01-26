804570a7a0581425da85be3374d709fd7242b647
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-01-26 21:59:39 +0100

    Regresja Logistyczna (Logistic Regression) - wprowadzenie

--- Nadzorowanie/03 Klasyfikacja/01_regresja_logistyczna.py
+++ Nadzorowanie/03 Klasyfikacja/01_regresja_logistyczna.py
@@ -1,7 +1,7 @@
 # Import bibliotek
 import numpy as np
 # import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import plotly.figure_factory as ff
 import seaborn as sns
 import sklearn
@@ -11,3 +11,18 @@ np.set_printoptions(precision=6, suppress=True, edgeitems=10, linewidth=100000,
                     formatter=dict(float=lambda x: f'{x:.2f}'))
 np.random.seed(42)
 print(sklearn.__version__)
+
+# Regresja Logistyczna (Logistic Regression) - wprowadzenie
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+X = np.arange(-5, 5, 0.1)
+y = sigmoid(X)
+
+plt.figure(figsize=(8, 6))
+plt.plot(X, y)
+plt.title('Funkcja Sigmoid')
+plt.show()
