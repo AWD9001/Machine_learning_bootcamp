0f9672772d6e703f6ea5fe2eca8532ec31bfb921
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-01-29 22:19:29 +0100

    Podział danych na zbiór treningowy i testowy

--- Nadzorowanie/03 Klasyfikacja/01_regresja_logistyczna.py
+++ Nadzorowanie/03 Klasyfikacja/01_regresja_logistyczna.py
@@ -43,3 +43,13 @@ target = all_data['target']
 
 print(f'rozmiar data: {data.shape}')
 print(f'rozmiar target: {target.shape}')
+
+# Podział danych na zbiór treningowy i testowy
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(data, target)
+
+print(f'X_train shape: {X_train.shape}')
+print(f'y_train shape: {y_train.shape}')
+print(f'X_test shape: {X_test.shape}')
+print(f'y_test shape: {y_test.shape}')
