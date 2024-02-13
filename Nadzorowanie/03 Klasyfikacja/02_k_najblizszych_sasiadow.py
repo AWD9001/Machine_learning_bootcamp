352d738860c969edb4cd8c0aea0f9bd0085a7602
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-02-13 22:14:12 +0100

    Import bibliotek

---
+++ Nadzorowanie/03 Klasyfikacja/02_k_najblizszych_sasiadow.py
@@ -0,0 +1,9 @@
+# Import bibliotek
+# import matplotlib.pyplot as plt
+import numpy as np
+# import pandas as pd
+# import plotly.express as px
+import seaborn as sns
+
+np.random.seed(42)
+sns.set(font_scale=1.3)
