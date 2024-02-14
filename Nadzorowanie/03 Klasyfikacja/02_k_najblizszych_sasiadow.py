25e6e47b61aa8f48707be4a9b2321345fc5b1473
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-02-14 21:48:03 +0100

    Wczytanie danych

--- Nadzorowanie/03 Klasyfikacja/02_k_najblizszych_sasiadow.py
+++ Nadzorowanie/03 Klasyfikacja/02_k_najblizszych_sasiadow.py
@@ -4,6 +4,11 @@ import numpy as np
 # import pandas as pd
 # import plotly.express as px
 import seaborn as sns
+from sklearn.datasets import load_iris
 
 np.random.seed(42)
 sns.set(font_scale=1.3)
+
+# Wczytanie danych
+raw_data = load_iris()
+print(raw_data.keys())
