00109ac4d5c90342c0afbb4f5c1f1494b6b80dbc
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-02-15 22:11:56 +0100

    Wczytanie danych

--- Nadzorowanie/03 Klasyfikacja/02_k_najblizszych_sasiadow.py
+++ Nadzorowanie/03 Klasyfikacja/02_k_najblizszych_sasiadow.py
@@ -12,3 +12,11 @@ sns.set(font_scale=1.3)
 # Wczytanie danych
 raw_data = load_iris()
 print(raw_data.keys())
+
+all_data = raw_data.copy()
+
+data = all_data['data']
+target = all_data['target']
+
+print(f'{data[:5]}\n')
+print(f'{target[:5]}')
