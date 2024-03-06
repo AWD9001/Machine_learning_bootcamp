57108d604abd7aa93ae91485bacf2ce69436174b
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-03-06 22:37:03 +0100

    Import bibliotek

---
+++ Nadzorowanie/03 Klasyfikacja/03_entropia_Giniego.py
@@ -0,0 +1,9 @@
+# Import bibliotek
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+# from IPython.display import Image
+
+sns.set(font_scale=1.3)
+np.random.seed(42)
