f91ca7fc10db219b4818398ba093957a95c7cb6e
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-03-26 22:15:45 +0100

    Import bibliotek

---
+++ Nadzorowanie/03 Klasyfikacja/04_drzewo_decyzyjne.py
@@ -0,0 +1,10 @@
+# Import bibliotek
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+from IPython.display import Image
+
+sns.set(font_scale=1.3)
+np.random.seed(42)
