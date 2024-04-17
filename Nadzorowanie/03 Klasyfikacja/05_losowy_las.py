8266b4d68a74a534972fab9f08fd458d64369918
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-04-17 21:28:10 +0200

    Załadowanie danych

--- Nadzorowanie/03 Klasyfikacja/05_losowy_las.py
+++ Nadzorowanie/03 Klasyfikacja/05_losowy_las.py
@@ -1,9 +1,21 @@
 # Import bibliotek
-import pandas as pd
+# import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
+
+# Załadowanie danych
+
+from sklearn.datasets import load_iris
+
+raw_data = load_iris()
+all_data = raw_data.copy()
+
+data = all_data['data']
+target = all_data['target']
+feature_names = all_data['feature_names']
+target_names = all_data['target_names']
