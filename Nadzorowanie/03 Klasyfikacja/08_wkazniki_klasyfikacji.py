cca128aff92ea61c4a39410c198c55aaff026035
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-06-14 23:28:26 +0200

    Metryki - Klasyfikacja binarna

--- Nadzorowanie/03 Klasyfikacja/08_wkazniki_klasyfikacji.py
+++ Nadzorowanie/03 Klasyfikacja/08_wkazniki_klasyfikacji.py
@@ -1,6 +1,14 @@
 # Import bibliotek
-# import numpy as np
+import numpy as np
 # import pandas as pd
 # import plotly.express as px
 # import plotly.graph_objects as go
 # from plotly.subplots import make_subplots
+
+# Metryki - Klasyfikacja binarna
+# Accuracy - Dokładność klasyfikacji
+# Accuracy = (correct predictions / total predictions) * 100
+y_true = np.array([1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1,
+                   1, 0, 1])
+y_pred = np.array([0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
+                   1, 0, 1])
