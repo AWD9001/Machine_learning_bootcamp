f2aaf42ae77283a0f8a7d3721af7c319c65ca322
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-06-15 23:41:12 +0200

    Accuracy - Dokładność klasyfikacji

--- Nadzorowanie/03 Klasyfikacja/08_wkazniki_klasyfikacji.py
+++ Nadzorowanie/03 Klasyfikacja/08_wkazniki_klasyfikacji.py
@@ -12,3 +12,7 @@ y_true = np.array([1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1
                    1, 0, 1])
 y_pred = np.array([0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1,
                    1, 0, 1])
+
+from sklearn.metrics import accuracy_score
+
+accuracy_score(y_true, y_pred)
