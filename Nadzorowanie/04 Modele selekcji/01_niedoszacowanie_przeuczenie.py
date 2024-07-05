e4fcce2dfd22ebfc7c644a57fc695d026530ffc9
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-07-05 23:11:47 +0200

    Import bibliotek

--- Nadzorowanie/04 Modele selekcji/01_niedoszacowanie_przeuczenie.py
+++ Nadzorowanie/04 Modele selekcji/01_niedoszacowanie_przeuczenie.py
@@ -3,8 +3,17 @@ import numpy as np
 # import pandas as pd
 # import matplotlib.pyplot as plt
 import seaborn as sns
-# from sklearn.datasets import make_moons
+from sklearn.datasets import make_moons
 # import plotly.express as px
 
 np.random.seed(42)
 sns.set(font_scale=1.3)
+
+# Wygenerowanie danych
+# n_samples=700
+raw_data = make_moons(n_samples=2000, noise=0.25, random_state=42)
+data = raw_data[0]
+target = raw_data[1]
+
+print(f'{data[:5]}\n')
+print(f'{target[:5]}')
