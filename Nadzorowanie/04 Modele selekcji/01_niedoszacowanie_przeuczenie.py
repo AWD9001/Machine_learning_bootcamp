91925bd7c85086f9b5b18beb6774167c7e5dc023
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-07-17 22:14:26 +0200

    Przykład przeuczenia

--- Nadzorowanie/04 Modele selekcji/01_niedoszacowanie_przeuczenie.py
+++ Nadzorowanie/04 Modele selekcji/01_niedoszacowanie_przeuczenie.py
@@ -79,3 +79,12 @@ plt.figure(figsize=(10, 8))
 plot_decision_regions(X_test, y_test, classifier)
 plt.title(f'Zbiór testowy: dokładność {classifier.score(X_test, y_test):.4f}')
 plt.show()
+
+# Przykład przeuczenia
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+plt.figure(figsize=(10, 8))
+plot_decision_regions(X_train, y_train, classifier)
+plt.title(f'Zbiór treningowy: dokładność {classifier.score(X_train, y_train):.4f}')
+plt.show()
