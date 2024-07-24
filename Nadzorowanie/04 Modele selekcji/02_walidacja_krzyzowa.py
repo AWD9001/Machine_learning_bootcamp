3ec641cce6c2d1ce19c6722650fae56277119840
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-07-24 23:12:55 +0200

    Import bibliotek

---
+++ Nadzorowanie/04 Modele selekcji/02_walidacja_krzyzowa.py
@@ -0,0 +1,10 @@
+# Import bibliotek
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+# from sklearn.datasets import make_moons
+# import plotly.express as px
+
+np.random.seed(42)
+sns.set(font_scale=1.3)
