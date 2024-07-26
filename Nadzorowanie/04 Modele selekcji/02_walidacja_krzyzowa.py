9a68a939c49fa8ac7c677af433534d429ba22dd1
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-07-26 22:52:32 +0200

    Wygenerowanie danych

--- Nadzorowanie/04 Modele selekcji/02_walidacja_krzyzowa.py
+++ Nadzorowanie/04 Modele selekcji/02_walidacja_krzyzowa.py
@@ -9,9 +9,13 @@ import seaborn as sns
 np.random.seed(42)
 sns.set(font_scale=1.3)
 
+# Wygenerowanie danych
 raw_data = make_moons(n_samples=700, noise=0.25, random_state=42)
 data = raw_data[0]
 target = raw_data[1]
 
 print(f'{data[:5]}\n')
 print(f'{target[:5]}')
+
+df = pd.DataFrame(data=np.c_[data, target], columns=['x1', 'x2', 'target'])
+df.head()
