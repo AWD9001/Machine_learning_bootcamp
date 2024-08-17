c13ca337bd095cc773b301430c1c8d8a3bb93845
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-08-17 22:59:37 +0200

    Import bibliotek

--- Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
+++ Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
@@ -1,1 +1,13 @@
 # Grid search
+
+# Import bibliotek
+
+# import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+# from sklearn.datasets import make_moons
+# import pandas as pd
+# import plotly.express as px
+
+np.random.seed(42)
+sns.set(font_scale=1.3)
