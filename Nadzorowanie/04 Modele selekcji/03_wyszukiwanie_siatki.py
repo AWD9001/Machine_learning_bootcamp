5dc62a80176224ead373819b7ba5c7b36c4b6bb7
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-08-18 22:58:38 +0200

    Wygenerowanie danych

--- Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
+++ Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
@@ -5,9 +5,17 @@
 # import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-# from sklearn.datasets import make_moons
+from sklearn.datasets import make_moons
 # import pandas as pd
 # import plotly.express as px
 
 np.random.seed(42)
 sns.set(font_scale=1.3)
+
+# Wygenerowanie danych
+raw_data = make_moons(n_samples=2000, noise=0.25, random_state=42)
+data = raw_data[0]
+target = raw_data[1]
+
+print(f'{data[:5]}\n')
+print(f'{target[:5]}')
