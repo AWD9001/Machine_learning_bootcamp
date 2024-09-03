079506280a5482021361978dfdf762debcc7bf2c
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-09-03 22:20:17 +0200

    Wybór optymalnych hiperparametrów - Grid Search

--- Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
+++ Nadzorowanie/04 Modele selekcji/03_wyszukiwanie_siatki.py
@@ -107,3 +107,8 @@ grid_search.fit(X_train, y_train)
 print(grid_search.best_params_)
 
 print(grid_search.best_estimator_)
+
+plt.figure(figsize=(10, 8))
+plot_decision_regions(X_test, y_test, grid_search)
+plt.title(f'Zbiór treningowy: dokładność {grid_search.score(X_train, y_train):.4f}')
+plt.show()
