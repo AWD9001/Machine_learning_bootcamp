7fb6a51b186636e38a7526443c8fd543cdb70bea
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-09-16 21:59:21 +0200

    Nowy program

---
+++ Nadzorowanie/05 Przypadki/01_Rozpoznawanie_kropek.py
@@ -0,0 +1,11 @@
+import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+# import plotly.figure_factory as ff
+# from sklearn import datasets
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+
+sns.set(font_scale=1.3)
+np.random.seed(42)
