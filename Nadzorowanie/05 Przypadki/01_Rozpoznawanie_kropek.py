59fd06ac3a6404608b4aece2791f2f8e613d9d6a
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-09-17 21:33:26 +0200

    Wczytanie danych

--- Nadzorowanie/05 Przypadki/01_Rozpoznawanie_kropek.py
+++ Nadzorowanie/05 Przypadki/01_Rozpoznawanie_kropek.py
@@ -3,9 +3,14 @@ import numpy as np
 # import matplotlib.pyplot as plt
 import seaborn as sns
 # import plotly.figure_factory as ff
-# from sklearn import datasets
+from sklearn import datasets
 # from sklearn.metrics import classification_report
 # from sklearn.metrics import confusion_matrix
 
 sns.set(font_scale=1.3)
 np.random.seed(42)
+
+# Wczytanie danych
+raw_digits = datasets.load_digits()
+digits = raw_digits.copy()
+digits.keys()
