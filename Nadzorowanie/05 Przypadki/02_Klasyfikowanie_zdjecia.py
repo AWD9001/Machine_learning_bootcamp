57e44a107bf489cfa7fa08fdd7ce2653dfe7f8e8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-10-13 22:51:15 +0200

    Import bibliotek

---
+++ Nadzorowanie/05 Przypadki/02_Klasyfikowanie_zdjecia.py
@@ -0,0 +1,11 @@
+# Import bibliotek
+# %tensorflow_version 2.x
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt
+import seaborn as sns
+# from tensorflow.keras.datasets.fashion_mnist import load_data
+
+np.set_printoptions(precision=12, suppress=True, linewidth=150)
+pd.options.display.float_format = '{:.6f}'.format
+sns.set(font_scale=1.3)
