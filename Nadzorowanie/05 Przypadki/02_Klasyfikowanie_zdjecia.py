3f7389af65243d959c15aabed780a573cf60f440
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-10-14 22:11:33 +0200

    Załadowanie danych i wstępna eksploracja

--- Nadzorowanie/05 Przypadki/02_Klasyfikowanie_zdjecia.py
+++ Nadzorowanie/05 Przypadki/02_Klasyfikowanie_zdjecia.py
@@ -4,8 +4,11 @@ import numpy as np
 import pandas as pd
 # import matplotlib.pyplot as plt
 import seaborn as sns
-# from tensorflow.keras.datasets.fashion_mnist import load_data
+from tensorflow.keras.datasets.fashion_mnist import load_data
 
 np.set_printoptions(precision=12, suppress=True, linewidth=150)
 pd.options.display.float_format = '{:.6f}'.format
 sns.set(font_scale=1.3)
+
+# Załadowanie danych i wstępna eksploracja
+(X_train, y_train), (X_test, y_test) = load_data()
