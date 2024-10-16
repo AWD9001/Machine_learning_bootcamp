d558584b3c931c9755b6da8bf632f236f4ab0469
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-10-16 22:32:21 +0200

    Załadowanie danych i wstępna eksploracja

--- Nadzorowanie/05 Przypadki/02_Klasyfikowanie_zdjecia.py
+++ Nadzorowanie/05 Przypadki/02_Klasyfikowanie_zdjecia.py
@@ -18,3 +18,5 @@ print(f'y_train shape: {y_train.shape}')
 print(f'X_test shape: {X_test.shape}')
 print(f'y_test shape: {y_test.shape}')
 print(f'X_train[0] shape: {X_train[0].shape}')
+
+print(X_train[0])
