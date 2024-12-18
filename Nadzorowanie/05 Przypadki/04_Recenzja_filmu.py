68b4a6b5052f07c76446e5746ea916122c3fce42
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2024-12-18 22:09:13 +0100

    Import bibliotek

--- Nadzorowanie/05 Przypadki/04_Recenzja_filmu.py
+++ Nadzorowanie/05 Przypadki/04_Recenzja_filmu.py
@@ -8,3 +8,7 @@ np.random.seed(42)
 np.set_printoptions(precision=6, suppress=True, edgeitems=10, linewidth=1000,
                     formatter=dict(float=lambda x: f'{x:.2f}'))
 print(sklearn.__version__)
+
+# Pobranie danych
+!wget https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/\
+              corpora/movie_reviews.zip
