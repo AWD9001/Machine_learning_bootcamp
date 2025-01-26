6a9937c9da25f1d6fc5b48b0b00805b87839afcb
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-01-26 23:26:48 +0100

    Implementacja algorytmu K-średnich

--- Unsuervised/01_clustering/01_kmeans_implementation.py
+++ Unsuervised/01_clustering/01_kmeans_implementation.py
@@ -38,3 +38,9 @@ centroid_1 = np.array([random.uniform(x1_min, x1_max), random.uniform(x2_min, x2
 centroid_2 = np.array([random.uniform(x1_min, x1_max), random.uniform(x2_min, x2_max)])
 print(centroid_1)
 print(centroid_2)
+
+# wizualizacja tzw. punktów startowych centroidów
+fig = px.scatter(df, 'x1', 'x2', width=950, height=500, title='Algorytm K-średnich - inicjalizacja centroidów')
+fig.add_trace(go.Scatter(x=[centroid_1[0]], y=[centroid_1[1]], name='centroid 1', mode='markers', marker_line_width=3))
+fig.add_trace(go.Scatter(x=[centroid_2[0]], y=[centroid_2[1]], name='centroid 2', mode='markers', marker_line_width=3))
+fig.update_traces(marker_size=12, showlegend=False)
