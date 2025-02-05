a5839dc1f44bb2a7edb5fb7e93d3954a9a9e5461
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-05 22:31:32 +0100

    Implementacja algorytmu K-średnich

--- Unsuervised/01_clustering/01_kmeans_implementation.py
+++ Unsuervised/01_clustering/01_kmeans_implementation.py
@@ -126,3 +126,17 @@ new_2_centroid_1 = [df[df.cluster == 1].x1.mean(), df[df.cluster == 1].x2.mean()
 new_2_centroid_2 = [df[df.cluster == 2].x1.mean(), df[df.cluster == 2].x2.mean()]
 
 print(new_2_centroid_1, new_2_centroid_2)
+
+# aktualizacja centroidów
+fig = px.scatter(df, 'x1', 'x2', color='cluster', width=950, height=500,
+                 title='Algorytm K-średnich - ponowne obliczenie centroidów')
+fig.add_trace(go.Scatter(x=[new_centroid_1[0]], y=[new_centroid_1[1]],
+                         name='centroid 1', mode='markers', marker_line_width=3))
+fig.add_trace(go.Scatter(x=[new_centroid_2[0]], y=[new_centroid_2[1]],
+                         name='centroid 2', mode='markers', marker_line_width=3))
+fig.add_trace(go.Scatter(x=[new_2_centroid_1[0]], y=[new_2_centroid_1[1]],
+                         name='centroid 1', mode='markers', marker_line_width=3))
+fig.add_trace(go.Scatter(x=[new_2_centroid_2[0]], y=[new_2_centroid_2[1]],
+                         name='centroid 2', mode='markers', marker_line_width=3))
+fig.update_traces(marker_size=12)
+fig.update_layout(showlegend=False)
