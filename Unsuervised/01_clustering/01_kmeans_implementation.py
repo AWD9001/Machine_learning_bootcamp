17d094191323a4605cbda8acfc9b34eff756cb43
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-09 22:48:14 +0100

    Implementacja algorytmu K - średnich - wizualizacja

--- Unsuervised/01_clustering/01_kmeans_implementation.py
+++ Unsuervised/01_clustering/01_kmeans_implementation.py
@@ -192,3 +192,15 @@ for i in range(10):
     centroid_2 = [df[df.cluster == 2].x1.mean(), df[df.cluster == 2].x2.mean()]
 
 print(new_centroid_1, new_centroid_2)
+
+# Implementacja algorytmu K - średnich - wizualizacja
+fig = px.scatter(df, 'x1', 'x2', color='cluster', width=950, height=500,
+                 title='Algorytm K-średnich - końcowy rezultat')
+fig.add_trace(
+    go.Scatter(x=[new_2_centroid_1[0]], y=[new_2_centroid_1[1]], name='centroid 1', mode='markers',
+               marker_line_width=3))
+fig.add_trace(
+    go.Scatter(x=[new_2_centroid_2[0]], y=[new_2_centroid_2[1]], name='centroid 2', mode='markers',
+               marker_line_width=3))
+fig.update_traces(marker_size=12)
+fig.update_layout(showlegend=False)
