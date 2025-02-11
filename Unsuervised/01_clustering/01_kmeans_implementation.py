89607ddb9888527f719664fa471525075e82b67e
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-11 23:05:02 +0100

    Implementacja algorytmu K - średnich - wizualizacja

--- Unsuervised/01_clustering/01_kmeans_implementation.py
+++ Unsuervised/01_clustering/01_kmeans_implementation.py
@@ -12,7 +12,8 @@ np.set_printoptions(precision=6)
 random.seed(41)
 
 # Wygenerowanie danych
-data = make_blobs(n_samples=40, centers=2, cluster_std=1.0, center_box=(-4.0, 4.0), random_state=42)[0]
+data = make_blobs(n_samples=40, centers=2, cluster_std=1.0,
+                  center_box=(-4.0, 4.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
 df.head()
 
