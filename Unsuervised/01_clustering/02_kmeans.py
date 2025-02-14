a2e1f445fef106bd89ec90582c44b5b322970917
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-14 22:21:56 +0100

    Wygenerowanie danych

--- Unsuervised/01_clustering/02_kmeans.py
+++ Unsuervised/01_clustering/02_kmeans.py
@@ -1,3 +1,11 @@
 # Import bibliotek
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
+
+# Wygenerowanie danych
+from sklearn.datasets import make_blobs
+
+data = make_blobs(n_samples=1000, centers=None, cluster_std=1.0,
+                  center_box=(-4.0, 4.0), random_state=42)[0]
+df = pd.DataFrame(data, columns=['x1', 'x2'])
+df.head()
