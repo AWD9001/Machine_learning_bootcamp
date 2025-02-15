e522e54a05d20d70c3d27d69629053b1eca4a2ec
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-15 23:07:01 +0100

    Wizualizacja danych

--- Unsuervised/01_clustering/02_kmeans.py
+++ Unsuervised/01_clustering/02_kmeans.py
@@ -9,3 +9,6 @@ data = make_blobs(n_samples=1000, centers=None, cluster_std=1.0,
                   center_box=(-4.0, 4.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
 df.head()
+
+# Wizualizacja danych
+px.scatter(df, 'x1', 'x2', width=950, height=500, title='Klasteryzacja - Algorytm K-średnich')
