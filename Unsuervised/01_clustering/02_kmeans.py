54752d0d32ed7135896461ff2992c73479936e87
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-20 21:06:00 +0100

    Wizualizacja klastrów

--- Unsuervised/01_clustering/02_kmeans.py
+++ Unsuervised/01_clustering/02_kmeans.py
@@ -1,11 +1,10 @@
 # Import bibliotek
 import pandas as pd
 import plotly.express as px
+from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
 
 # Wygenerowanie danych
-from sklearn.datasets import make_blobs
-
 data = make_blobs(n_samples=1000, centers=None, cluster_std=1.0,
                   center_box=(-4.0, 4.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
@@ -15,8 +14,8 @@ df.head()
 px.scatter(df, 'x1', 'x2', width=950, height=500, title='Klasteryzacja - Algorytm K-średnich')
 
 # Algorytm K-średnich
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(data)
+y_kmeans = KMeans(n_clusters=4)
+y_kmeans.fit(data)
 
 df['y_kmeans'] = y_kmeans
 df.head()
