6be48abe2034aed28be53902b82b42cc9a8f0814
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-22 22:42:09 +0100

    Import bibliotek

---
+++ Unsuervised/01_clustering/03_kmeans_elbow_method.py
@@ -0,0 +1,8 @@
+# Import bibliotek
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(font_scale=1.3)
