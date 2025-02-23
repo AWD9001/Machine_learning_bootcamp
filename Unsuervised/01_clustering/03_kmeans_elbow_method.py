32ccabad8ce3807f610c728f28b84f5e45f31e0e
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-23 22:25:09 +0100

    Wygenerowanie danych

--- Unsuervised/01_clustering/03_kmeans_elbow_method.py
+++ Unsuervised/01_clustering/03_kmeans_elbow_method.py
@@ -6,3 +6,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set(font_scale=1.3)
+
+# Wygenerowanie danych
+from sklearn.datasets import make_blobs
+
+data = make_blobs(n_samples=1000, centers=4, cluster_std=1.5, center_box=(-8.0, 8.0), random_state=42)[0]
+df = pd.DataFrame(data, columns=['x1', 'x2'])
+
+px.scatter(df, 'x1', 'x2', width=950, height=500, title='Algorytm K-średnich', template='plotly_dark')
