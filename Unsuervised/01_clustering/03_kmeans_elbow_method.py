20b4e039d4fccb1702a69e4eb29ac66af00943ba
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-02-24 22:08:30 +0100

    Algorytm K-średnich

--- Unsuervised/01_clustering/03_kmeans_elbow_method.py
+++ Unsuervised/01_clustering/03_kmeans_elbow_method.py
@@ -10,7 +10,15 @@ sns.set(font_scale=1.3)
 # Wygenerowanie danych
 from sklearn.datasets import make_blobs
 
-data = make_blobs(n_samples=1000, centers=4, cluster_std=1.5, center_box=(-8.0, 8.0), random_state=42)[0]
+data = make_blobs(n_samples=1000, centers=4, cluster_std=1.5,
+                  center_box=(-8.0, 8.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
 
-px.scatter(df, 'x1', 'x2', width=950, height=500, title='Algorytm K-średnich', template='plotly_dark')
+px.scatter(df, 'x1', 'x2', width=950, height=500, title='Algorytm K-średnich',
+           template='plotly_dark')
+
+# Algorytm K-średnich
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(data)
