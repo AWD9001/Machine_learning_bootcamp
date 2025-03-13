9283c40dace78759f86d2a014876431ce4ed97ff
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-03-13 21:35:12 +0100

    Wygenerowanie danych

--- Unsuervised/01_clustering/04_hierarchical_clustering.py
+++ Unsuervised/01_clustering/04_hierarchical_clustering.py
@@ -6,3 +6,11 @@ import seaborn as sns
 import plotly.express as px
 
 sns.set(font_scale=1.2)
+
+# Wygenerowanie danych
+from sklearn.datasets import make_blobs
+
+data = make_blobs(n_samples=10, centers=2, cluster_std=1.0,
+                  center_box=(-8.0, 8.0), random_state=42)[0]
+df = pd.DataFrame(data, columns=['x1', 'x2'])
+df.head()
