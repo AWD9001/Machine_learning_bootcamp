fcebefcc5952f6693c130d30f248a0a9ac718f61
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-03-14 23:09:05 +0100

    Wizualizacja danych

--- Unsuervised/01_clustering/04_hierarchical_clustering.py
+++ Unsuervised/01_clustering/04_hierarchical_clustering.py
@@ -14,3 +14,13 @@ data = make_blobs(n_samples=10, centers=2, cluster_std=1.0,
                   center_box=(-8.0, 8.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
 df.head()
+
+# Wizualizacja danych
+plt.figure(figsize=(14, 7))
+plt.scatter(data[:,0], data[:,1])
+
+for label, x, y in zip(range(1, 11), data[:, 0], data[:, 1]):
+    plt.annotate(label, xy=(x, y), xytext=(-3, 3), textcoords='offset points',
+                 ha='right', va='bottom')
+plt.title('Grupowanie hierarchiczne')
+plt.show()
