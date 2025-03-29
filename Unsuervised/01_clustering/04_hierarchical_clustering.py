ff1b67713147f25342258c010a782378105dd33f
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-03-29 22:19:18 +0100

    Porównanie metryk (euklidesowa, Manhattan, kosinusowa)

--- Unsuervised/01_clustering/04_hierarchical_clustering.py
+++ Unsuervised/01_clustering/04_hierarchical_clustering.py
@@ -4,13 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.datasets import make_blobs
 import plotly.express as px
 
 sns.set(font_scale=1.2)
 
 # Wygenerowanie danych
-from sklearn.datasets import make_blobs
-
 data = make_blobs(n_samples=10, centers=2, cluster_std=1.0,
                   center_box=(-8.0, 8.0), random_state=42)[0]
 df = pd.DataFrame(data, columns=['x1', 'x2'])
