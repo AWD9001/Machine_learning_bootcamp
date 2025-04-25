b03808c069ccddcbf6e8a208ec27e6b4c0bfd13f
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-04-25 23:46:56 +0200

    Porównanie algorytmów - cirlce data - 2 klastry

--- Unsuervised/01_clustering/06_clustering_comparison.py
+++ Unsuervised/01_clustering/06_clustering_comparison.py
@@ -54,3 +54,32 @@ fig.add_trace(trace3, row=1, col=3)
 fig.update_layout(title='KMeans vs. Agglomerative Clustering vs. DBSCAN - blobs data',
                   template='plotly_dark', coloraxis={'colorscale': 'viridis'})
 fig.show()
+
+# Porównanie algorytmów - cirlce data - 2 klastry
+fig = make_subplots(rows=1, cols=3, shared_yaxes=True, horizontal_spacing=0.01)
+
+# KMeans
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(circle_data)
+clusters = kmeans.predict(circle_data)
+circle['cluster'] = clusters
+trace1 = px.scatter(circle, 'x1', 'x2', 'cluster', width=800, height=500)['data'][0]
+fig.add_trace(trace1, row=1, col=1)
+
+# AgglomerativeClustering
+agglo = AgglomerativeClustering(n_clusters=3, affinity='euclidean')
+clusters = agglo.fit_predict(circle_data)
+circle['cluster'] = clusters
+trace2 = px.scatter(circle, 'x1', 'x2', 'cluster', width=800, height=500)['data'][0]
+fig.add_trace(trace2, row=1, col=2)
+
+# DBSCAN
+dbscan = DBSCAN(eps=0.1, min_samples=5)
+dbscan.fit(circle_data)
+clusters = dbscan.labels_
+circle['cluster'] = clusters
+trace3 = px.scatter(circle, 'x1', 'x2', 'cluster', width=800, height=500)['data'][0]
+fig.add_trace(trace3, row=1, col=3)
+
+fig.update_layout(title='KMeans vs. Agglomerative Clustering vs. DBSCAN - circle data',
+                  template='plotly_dark', coloraxis = {'colorscale':'viridis'})
