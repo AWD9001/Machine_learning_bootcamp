b5300dd764c20bafcd9055ededfef658bb63120d
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-04-28 23:24:15 +0200

    Porównanie algorytmów - random data

--- Unsuervised/01_clustering/06_clustering_comparison.py
+++ Unsuervised/01_clustering/06_clustering_comparison.py
@@ -2,10 +2,9 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
-
-# Wygenerowanie danych i wizualizacja
 from sklearn.datasets import make_blobs
 
+# Wygenerowanie danych i wizualizacja
 blobs_data = make_blobs(n_samples=1000, cluster_std=0.7, random_state=24, center_box=(-4.0, 4.0))[0]
 blobs = pd.DataFrame(blobs_data, columns=['x1', 'x2'])
 px.scatter(blobs, 'x1', 'x2', width=950, height=500, title='blobs data', template='plotly_dark')
