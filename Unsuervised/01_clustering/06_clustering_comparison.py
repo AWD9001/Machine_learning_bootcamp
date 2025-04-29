85452cafe7e6901710709c4df7e4205f0d3def84
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-04-29 23:39:42 +0200

    Porównanie algorytmów - random data

--- Unsuervised/01_clustering/06_clustering_comparison.py
+++ Unsuervised/01_clustering/06_clustering_comparison.py
@@ -2,14 +2,13 @@
 import numpy as np
 import pandas as pd
 import plotly.express as px
+from sklearn.datasets import make_circles
 from sklearn.datasets import make_blobs
 
 # Wygenerowanie danych i wizualizacja
 blobs_data = make_blobs(n_samples=1000, cluster_std=0.7, random_state=24, center_box=(-4.0, 4.0))[0]
 blobs = pd.DataFrame(blobs_data, columns=['x1', 'x2'])
 px.scatter(blobs, 'x1', 'x2', width=950, height=500, title='blobs data', template='plotly_dark')
-
-from sklearn.datasets import make_circles
 
 circle_data = make_circles(n_samples=1000, factor=0.5, noise=0.05)[0]
 circle = pd.DataFrame(circle_data, columns=['x1', 'x2'])
