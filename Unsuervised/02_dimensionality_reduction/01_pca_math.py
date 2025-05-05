0896e331ba44012e4b097b62f1667e3e3357bd9f
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-05-05 22:49:46 +0200

    Import bibliotek

---
+++ Unsuervised/02_dimensionality_reduction/01_pca_math.py
@@ -0,0 +1,8 @@
+# Import bibliotek
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+
+np.set_printoptions(precision=8, suppress=True, edgeitems=5, linewidth=200)
