01de4dade8bd00cceac83e7dc82fe9d1b61dd4af
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-05-30 22:47:25 +0200

    PCA - scikit-learn

--- Unsuervised/02_dimensionality_reduction/01_pca_math.py
+++ Unsuervised/02_dimensionality_reduction/01_pca_math.py
@@ -1,7 +1,6 @@
 # Import bibliotek
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import plotly.express as px
 from sklearn.datasets import load_iris
