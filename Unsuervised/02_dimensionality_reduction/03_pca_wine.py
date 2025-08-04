9b5f27e2d7b662b7bba1ba3c6cff583050c5be48
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-08-04 22:03:51 +0200

    Nowy program

---
+++ Unsuervised/02_dimensionality_reduction/03_pca_wine.py
@@ -0,0 +1,7 @@
+# Import bibliotek
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+
+np.set_printoptions(precision=4, suppress=True, edgeitems=5, linewidth=200)
