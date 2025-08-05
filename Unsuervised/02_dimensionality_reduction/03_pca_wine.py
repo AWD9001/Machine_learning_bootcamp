a3d77e9ecfc98eeba68cbf553c63e02c50f1d9ea
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-08-05 22:40:55 +0200

    Nowy program

--- Unsuervised/02_dimensionality_reduction/03_pca_wine.py
+++ Unsuervised/02_dimensionality_reduction/03_pca_wine.py
@@ -5,3 +5,10 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 np.set_printoptions(precision=4, suppress=True, edgeitems=5, linewidth=200)
+
+# Załadowanie danych
+
+df_raw = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+                     header=None)
+df = df_raw.copy()
+df.head()
