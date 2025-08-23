0cab0e9b5b22e5da90f46ebb4166b6f057de190c
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-08-23 22:13:26 +0200

    PCA

--- Unsuervised/02_dimensionality_reduction/03_pca_wine.py
+++ Unsuervised/02_dimensionality_reduction/03_pca_wine.py
@@ -10,12 +10,10 @@ from sklearn.preprocessing import StandardScaler
 np.set_printoptions(precision=4, suppress=True, edgeitems=5, linewidth=200)
 
 # Załadowanie danych
-
 df_raw = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
                      header=None)
 df = df_raw.copy()
 df.head()
-
 
 data = df.iloc[:, 1:]
 target = df.iloc[:, 0]
