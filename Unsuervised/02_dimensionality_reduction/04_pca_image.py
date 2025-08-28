9eb4cf394cdc67242fe0022ec9418d512624c0a4
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-08-28 22:40:53 +0200

    Wygenerowanie danych

--- Unsuervised/02_dimensionality_reduction/04_pca_image.py
+++ Unsuervised/02_dimensionality_reduction/04_pca_image.py
@@ -5,7 +5,6 @@ import plotly.graph_objects as go
 from keras.datasets import mnist
 
 # Wygenerowanie danych
-
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 print(f'X_train shape: {X_train.shape}')
