e242258a6b6aaab9caa05084b5c7043ed7ff33f8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-09-15 22:07:29 +0200

    Import bibliotek

---
+++ Unsuervised/02_dimensionality_reduction/05_tsne.py
@@ -0,0 +1,9 @@
+# Import bibliotek
+import pandas as pd
+import numpy as np
+from keras.datasets import mnist
+import plotly.graph_objects as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
