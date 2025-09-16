55ae3ad3695dc73f93feadda3c49da58287de82c
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-09-16 22:01:37 +0200

    Import bibliotek

--- Unsuervised/02_dimensionality_reduction/05_tsne.py
+++ Unsuervised/02_dimensionality_reduction/05_tsne.py
@@ -7,3 +7,11 @@ import plotly.express as px
 import matplotlib.pyplot as plt
 
 np.random.seed(42)
+
+# Załadowanie danych
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print(f'X_train shape: {X_train.shape}')
+print(f'X_test shape: {X_test.shape}')
+print(f'y_train shape: {y_train.shape}')
+print(f'y_test shape: {y_test.shape}')
