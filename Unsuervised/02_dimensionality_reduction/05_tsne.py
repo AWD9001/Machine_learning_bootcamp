d9c509baa5a242c9a1f4c7ce69a9c14505e1b9ab
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-10-16 23:03:58 +0200

    50 komponentów

--- Unsuervised/02_dimensionality_reduction/05_tsne.py
+++ Unsuervised/02_dimensionality_reduction/05_tsne.py
@@ -1,7 +1,7 @@
 # Import bibliotek
+from keras.datasets import mnist
 import pandas as pd
 import numpy as np
-from keras.datasets import mnist
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
