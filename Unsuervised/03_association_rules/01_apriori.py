a6abd13308bee31da885704eeaea8082c6d0940a
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-10-23 22:32:13 +0200

    Import bibliotek

--- Unsuervised/03_association_rules/01_apriori.py
+++ Unsuervised/03_association_rules/01_apriori.py
@@ -1,3 +1,9 @@
 # Import bibliotek
 import numpy as np
 import pandas as pd
+
+# Wygenerowanie danych
+data = {'produkty': ['chleb jajka mleko', 'mleko ser', 'chleb masło ser', 'chleb jajka']}
+
+transactions = pd.DataFrame(data=data, index=[1, 2, 3, 4])
+print(transactions)
