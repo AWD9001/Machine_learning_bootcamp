5dc7b3a22502fc81e30ec533f415601409a931cf
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-10-24 23:23:10 +0200

    Import bibliotek

--- Unsuervised/03_association_rules/01_apriori.py
+++ Unsuervised/03_association_rules/01_apriori.py
@@ -7,3 +7,8 @@ data = {'produkty': ['chleb jajka mleko', 'mleko ser', 'chleb masło ser', 'chle
 
 transactions = pd.DataFrame(data=data, index=[1, 2, 3, 4])
 print(transactions)
+
+# Przygotowanie danych
+# rozwinięcie kolumny do obiektu DataFrame
+expand = transactions['produkty'].str.split(expand=True)
+print(expand)
