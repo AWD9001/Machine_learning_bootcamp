f695a06d058426f1225b78b458663c7473c7f705
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-10-25 21:55:23 +0200

    Import bibliotek

--- Unsuervised/03_association_rules/01_apriori.py
+++ Unsuervised/03_association_rules/01_apriori.py
@@ -12,3 +12,14 @@ print(transactions)
 # rozwinięcie kolumny do obiektu DataFrame
 expand = transactions['produkty'].str.split(expand=True)
 print(expand)
+
+
+# wydobycie nazw wszystkich produktów
+products = []
+for col in expand.columns:
+    for product in expand[col].unique():
+        if product is not None and product not in products:
+            products.append(product)
+
+products.sort()
+print(products)
