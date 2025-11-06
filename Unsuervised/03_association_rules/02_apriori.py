428d1f3c42ecaee63a8709e18f59acc55454f9f8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-11-06 22:50:35 +0100

    Nowy program

--- Unsuervised/03_association_rules/02_apriori.py
+++ Unsuervised/03_association_rules/02_apriori.py
@@ -2,3 +2,8 @@
 import pandas as pd
 
 pd.set_option('display.float_format', lambda x: f'{x:.2f}')
+
+# Załadownaie danych
+!wget https://storage.googleapis.com/esmartdata-courses-files/ml-course/products.csv
+!wget https://storage.googleapis.com/esmartdata-courses-files/ml-course/orders.csv
+
