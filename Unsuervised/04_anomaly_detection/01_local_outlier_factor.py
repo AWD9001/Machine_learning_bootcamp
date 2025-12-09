f29eca1880fb0fdd06be87a03c9377dbe40c1668
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-12-09 22:17:52 +0100

    Import bibliotek

---
+++ Unsuervised/04_anomaly_detection/01_local_outlier_factor.py
@@ -0,0 +1,11 @@
+# Import bibliotek
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import make_blobs
+import plotly.express as px
+import plotly.graph_objects as go
+
+sns.set(font_scale=1.2)
+np.random.seed(10)
