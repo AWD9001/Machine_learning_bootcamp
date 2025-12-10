09c3a0dfcd2f418a13161b6570ef9f13f48f01b8
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-12-10 22:39:15 +0100

    Wygenerowanie danych

--- Unsuervised/04_anomaly_detection/01_local_outlier_factor.py
+++ Unsuervised/04_anomaly_detection/01_local_outlier_factor.py
@@ -9,3 +9,7 @@ import plotly.graph_objects as go
 
 sns.set(font_scale=1.2)
 np.random.seed(10)
+
+# Wygenerowanie danych
+data = make_blobs(n_samples=300, cluster_std=2.0, random_state=10)[0]
+data[:5]
