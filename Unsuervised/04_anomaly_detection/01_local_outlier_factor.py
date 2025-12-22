51a91ff204b56c66150fe48461937f87808b213c
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2025-12-22 22:53:27 +0100

    Wizualizacja danych

--- Unsuervised/04_anomaly_detection/01_local_outlier_factor.py
+++ Unsuervised/04_anomaly_detection/01_local_outlier_factor.py
@@ -64,3 +64,12 @@ plt.title('Local Outlier Factor')
 legend = plt.legend()
 legend.legendHandles[1]._sizes = [40]
 plt.show()
+
+plt.figure(figsize=(12, 7))
+plt.scatter(all_data[:, 0], all_data[:, 1], c=all_data[:, 2], cmap='tab10', label='data')
+plt.scatter(all_data[:, 0], all_data[:, 1], s=2000 * radius, edgecolors='r', facecolors='none',
+            label='outlier scores')
+plt.title('Local Outlier Factor')
+legend = plt.legend()
+legend.legendHandles[1]._sizes = [40]
+plt.show()
