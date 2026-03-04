b00316a5ac36f2282b751a4de46e1af6fa849f3f
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2026-03-04 21:42:14 +0100

    Załadowanie danych

--- Unsuervised/05_case_studies/01_customer_segmentation.py
+++ Unsuervised/05_case_studies/01_customer_segmentation.py
@@ -4,3 +4,10 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+
+# Załadowanie danych
+
+url = 'https://storage.googleapis.com/esmartdata-courses-files/ml-course/OnlineRetail.csv'
+raw_data = pd.read_csv(url, encoding='latin', parse_dates=['InvoiceDate'])
+data = raw_data.copy()
+data.head(3)
