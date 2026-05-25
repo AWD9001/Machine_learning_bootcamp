cd891ebe0cd9919d3cf779c192b15480e623d939
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2026-05-25 22:26:07 +0200

    Sprzedaż - KMeans

--- Unsuervised/05_case_studies/01_customer_segmentation.py
+++ Unsuervised/05_case_studies/01_customer_segmentation.py
@@ -289,3 +289,6 @@ print(wcss)
 px.line(wcss, x='NumberOfClusters', y='WCSS', template='plotly_dark',
         color_discrete_sequence=['#03fcb5'],
         width=950, title='WCSS')
+
+kmeans = KMeans(n_clusters=3, max_iter=1000)
+kmeans.fit(data_sales_scaled)
