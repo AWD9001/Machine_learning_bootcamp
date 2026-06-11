61877f23ca35bd63ea7cf6a3988fc2e2778acd31
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2026-06-11 22:01:25 +0200

    Retencja, sprzedaż - KMeans

--- Unsuervised/05_case_studies/01_customer_segmentation.py
+++ Unsuervised/05_case_studies/01_customer_segmentation.py
@@ -322,3 +322,12 @@ px.scatter(data_user, x='RetentionScaled', y='SalesScaled', template='plotly_dar
 
 data_scaled = data_user[['RetentionScaled', 'SalesScaled']]
 data_scaled.head()
+
+wcss = []
+for i in range(1, 10):
+    kmeans = KMeans(n_clusters=i, max_iter=1000)
+    kmeans.fit(data_scaled)
+    wcss.append(kmeans.inertia_)
+
+wcss = pd.DataFrame(data=np.c_[range(1, 10), wcss], columns=['NumberOfClusters', 'WCSS'])
+print(wcss)
