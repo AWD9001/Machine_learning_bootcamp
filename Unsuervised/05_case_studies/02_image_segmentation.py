693e5447bb463856a2223ea172cb1b26a3cf09eb
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2026-07-29 22:28:47 +0200

    Pobranie obrazów

--- Unsuervised/05_case_studies/02_image_segmentation.py
+++ Unsuervised/05_case_studies/02_image_segmentation.py
@@ -3,3 +3,11 @@ import numpy as np
 import pandas as pd
 import cv2
 from google.colab.patches import cv2_imshow
+
+#Pobranie obrazów
+!wget
+https: // storage.googleapis.com / esmartdata - courses - files / ml - course / green.jpg
+!wget
+https: // storage.googleapis.com / esmartdata - courses - files / ml - course / ski.jpg
+!wget
+https: // storage.googleapis.com / esmartdata - courses - files / ml - course / view.jpg
