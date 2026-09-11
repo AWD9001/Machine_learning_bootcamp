2e34b17a436e364741a188a0928487b7d95af625
Michał Pasternakiewicz <michal.pasternakiewicz@o2.pl>
2026-09-11 23:50:53 +0200

    Eksploracja i przygotowanie danych

--- Unsuervised/05_case_studies/03_coronavirus.py
+++ Unsuervised/05_case_studies/03_coronavirus.py
@@ -79,4 +79,4 @@ fig = go.Figure()
 fig.add_trace(go.Scatter(x=data_confirmed['ds'], y=data_confirmed['y'], mode='markers+lines',
                          name='Confirmed', fill='tozeroy'))
 fig.update_layout(template='plotly_dark', width=950,
-                  title='Liczba potwierdzonych przypadków (22.01-12.02)')
+                  title='Liczba potwierdzonych przypadków (22.01-12.021)')
